4b93d38dd8d82acfe6a1dae0edc80e3f8a37306a
Conley <conleyscomputergreek@outlook.com>
2026-04-12 10:45:03 -0500

    Initial commit

---
+++ src/client/java/io/github/ratdotjar/btf/client/BetterThanFrogsClient.java
@@ -0,0 +1,10 @@
+package io.github.ratdotjar.btf.client;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class BetterThanFrogsClient implements ClientModInitializer {
+	@Override
+	public void onInitializeClient() {
+		// This entrypoint is suitable for setting up client-specific logic, such as rendering.
+	}
+}
---
+++ src/client/java/io/github/ratdotjar/btf/client/BetterThanFrogsDataGenerator.java
@@ -0,0 +1,11 @@
+package io.github.ratdotjar.btf.client;
+
+import net.fabricmc.fabric.api.datagen.v1.DataGeneratorEntrypoint;
+import net.fabricmc.fabric.api.datagen.v1.FabricDataGenerator;
+
+public class BetterThanFrogsDataGenerator implements DataGeneratorEntrypoint {
+	@Override
+	public void onInitializeDataGenerator(FabricDataGenerator fabricDataGenerator) {
+
+	}
+}
