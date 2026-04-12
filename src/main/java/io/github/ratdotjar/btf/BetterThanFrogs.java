c341dd24bed0315190688cb21c70130db7f7d7a5
Conley <conleyscomputergreek@outlook.com>
2026-04-12 11:01:18 -0500

    idk

--- src/main/java/io/github/ratdotjar/btf/BetterThanFrogs.java
+++ src/main/java/io/github/ratdotjar/btf/BetterThanFrogs.java
@@ -6,7 +6,7 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 public class BetterThanFrogs implements ModInitializer {
-	public static final String MOD_ID = "better-than-frogs";
+	public static final String MOD_ID = "btf";
 
 	// This logger is used to write text to the console and the log file.
 	// It is considered best practice to use your mod id as the logger's name.
