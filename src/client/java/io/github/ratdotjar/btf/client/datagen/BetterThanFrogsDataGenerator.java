456253345726c6a83400d241d679d9aa567bed2a
Conley <conleyscomputergreek@outlook.com>
2026-04-13 17:58:04 -0500

    Added data generation, renewable sponges, and made it so librarians can only sell non treasure enchantments.

--- src/client/java/io/github/ratdotjar/btf/client/datagen/BetterThanFrogsDataGenerator.java
+++ src/client/java/io/github/ratdotjar/btf/client/datagen/BetterThanFrogsDataGenerator.java
@@ -1,4 +1,4 @@
-package io.github.ratdotjar.btf.client;
+package io.github.ratdotjar.btf.client.datagen;
 
 import net.fabricmc.fabric.api.datagen.v1.DataGeneratorEntrypoint;
 import net.fabricmc.fabric.api.datagen.v1.FabricDataGenerator;
@@ -6,6 +6,6 @@ import net.fabricmc.fabric.api.datagen.v1.FabricDataGenerator;
 public class BetterThanFrogsDataGenerator implements DataGeneratorEntrypoint {
 	@Override
 	public void onInitializeDataGenerator(FabricDataGenerator fabricDataGenerator) {
-
+		FabricDataGenerator.Pack pack = fabricDataGenerator.createPack();
 	}
 }
